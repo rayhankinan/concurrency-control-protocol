a1340f79e1e928390f9e5716d52655a6dbbb3176
rayhankinan <rayhankinan@gmail.com>
2022-11-27 23:30:17 +0700

    Initialize concurrency control

--- manager/ConcurrencyControl.py
+++ manager/ConcurrencyControl.py
@@ -1,3 +1,11 @@
+from typing import List
+from process.Transaction import Transaction
+
+
 class ConcurrencyControl:
-    def run(self):
+    def __init__(self, listOfTransaction: List[Transaction], schedule: List[int]) -> None:
+        self.listOfTransaction = listOfTransaction
+        self.schedule = schedule
+
+    def run():
         raise NotImplementedError()
